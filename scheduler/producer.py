60cd44d7dcd944b3ef76786caa6dd5a95d77ab3e
Tanapat Tiptaingtae <caveman.pk@gmail.com>
2026-09-15 10:33:12 +0700

    flake8 #1

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,27 +1,31 @@
 import pika
 import os
 
+
 def produce(host, username, password, body):
-    #Create plain credentials
+    # Create plain credentials
     credentials = pika.PlainCredentials(username, password)
 
     parameters = pika.ConnectionParameters(host=host, credentials=credentials)
-    connection = pika.BlockingConnection(parameters) #synchronus connection with localhost
+    # synchronus connection with localhost
+    connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.exchange_declare(exchange="jobs", exchange_type="direct") #create exchange named jobs with 'direct' type
-    channel.queue_declare(queue="router_jobs") #create queue named router_jobs for storing messages
-    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces") #bind jobs (exchange) with router_jobs (queue)
-    #with routing keys as 'check_interfaces'
-
+    # create exchange named jobs with 'direct' type
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    # create queue named router_jobs for storing messages
+    channel.queue_declare(queue="router_jobs")
+    # bind jobs (exchange) with router_jobs (queue)
+    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+    # with routing keys as 'check_interfaces'
     channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body) #message publish testing, body=payload
-    #sent message '192.168.1.44' to exchange 'jobs' with routing_keys 'check_interfaces'
-
     connection.close()
 
+
 if __name__ == "__main__":
-    rabbitmq_host = os.getenv("RABBITMQ_HOST", "localhost") #the second parameter is just in case var.
+    # the second parameter is just in case var.
+    rabbitmq_host = os.getenv("RABBITMQ_HOST", "localhost") 
     rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER", "admin")
     rabbitmq_pass = os.getenv("RABBITMQ_DEFAULT_PASS", "rabbitmq")
-
-    produce(rabbitmq_host, rabbitmq_user, rabbitmq_pass,"192.168.1.44") #rabbitmq means rabbitmq container in network
+    # rabbitmq means rabbitmq container in network
+    produce(rabbitmq_host, rabbitmq_user, rabbitmq_pass,"192.168.1.44") 
