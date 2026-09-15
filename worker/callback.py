a69bcbb363fc1ae6ebd12839793dc0ee1586d970
Tanapat Tiptaingtae <caveman.pk@gmail.com>
2026-09-15 10:18:45 +0700

    add ci.yml

--- worker/callback.py
+++ worker/callback.py
@@ -1,5 +1,15 @@
+from datetime import datetime
 from bson import json_util
 from router_client import get_interfaces
+
+#Preparing for saving the SSH result to database
+from pymongo import MongoClient
+import os
+mongo_uri = os.environ.get("MONGO_URI", "mongodb://127.0.0.1:27017/")
+db_name = os.environ.get("DB_NAME", "ipa2026_db")
+client = MongoClient(mongo_uri)
+db = client[db_name]
+collection = db["routers"]
 
 def callback(ch, method, props, body):
     job = json_util.loads(body.decode())
@@ -9,6 +19,22 @@ def callback(ch, method, props, body):
     print(f"Received job for router {router_ip}")
 
     try:
-        output = get_interfaces(router_ip, router_username, router_password)
+        result = get_interfaces(router_ip, router_username, router_password)
+        if result:
+            collection.update_one(
+                {"ip": router_ip},
+                {
+                    "$push": {
+                        "history": {
+                            "timestamp": datetime.now(),
+                            "interfaces": result
+                        }
+                    }
+                },
+                upsert=True # ถ้ายังไม่มี IP นี้ใน DB ให้สร้าง Document ใหม่ตั้งต้นให้เลย
+            )
+            print(f" เพิ่มประวัติ interfaces ของ {router_ip} ลงใน history สำเร็จ!")
+        else:
+            print(f" ไม่พบข้อมูล interfaces จาก {router_ip}")
     except Exception as e:
         print(f" Error: {e}")
